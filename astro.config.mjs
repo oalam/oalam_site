97f03c14297e75f97012a7b28a194b8ba7ce8d60
tom <thomas.bailet@hurence.com>
2026-03-08 21:30:17 +0100

    fix

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,8 +1,6 @@
 import { defineConfig } from 'astro/config';
-import sitemap from '@astrojs/sitemap';
 
 export default defineConfig({
   site: 'https://oalam.org',
   output: 'static',
-  integrations: [sitemap()],
 });
